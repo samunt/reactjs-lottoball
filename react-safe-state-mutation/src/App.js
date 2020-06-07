02dd227b3480e64173953ac3a19e44a7b5612850
samunt <sammy.unterman@gmail.com>
2020-06-07 14:54:37 -0400

    safe state mutation reactjs

---
+++ react-safe-state-mutation/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Iconlist from './Iconlist';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Iconlist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-safe-state-mutation/src/Iconlist.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Iconlist extends Component {
+    static defaultProps = {
+        options: [
+            "angry",
+            "anchor",
+            "archive",
+            "at",
+            "archway",
+            "baby",
+            "bell",
+            "bolt",
+            "bone",
+            "car",
+            "city",
+            "cloud",
+            "couch"
+        ]
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            icons: ['bone']
+        };
+        this.addIcon = this.addIcon.bind(this)
+    }
+    addIcon() {
+        let idx = Math.floor(Math.random() * this.props.options.length);
+        let newIcon = this.props.options[idx]
+        this.setState({icons: [ ...this.state.icons, newIcon ]})
+    }
+    render() {
+        const icons = this.state.icons.map(i => <i className={`fas fa-${i}`}/>);
+        return (
+            <div>
+                <h1>Icons: {icons}</h1>
+                <button onClick={this.addIcon}>Add Icon</button>
+            </div>
+
+        )
+    }
+}
+export default Iconlist;
